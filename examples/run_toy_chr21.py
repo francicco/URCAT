42ee1caa0698ba7c44afca10efad3e4c7deff20b
Francesco Cicconardi <francicco@gmail.com>
2026-03-16 23:27:44 +0000

    Update run_toy_chr21.py

--- examples/run_toy_chr21.py
+++ examples/run_toy_chr21.py
@@ -5,25 +5,25 @@ from comparative_annotator.pipeline.infer_locus import infer_comparative_locus
 
 
 def main():
-    hmel = load_gff3("/mnt/data/Hmel202001o.test.gff3", species="Hmel202001o")
-    eisa = load_gff3("/mnt/data/Eisa2100.test.gff3", species="Eisa2100")
-    diul = load_gff3("/mnt/data/Diul2100.test.gff3", species="Diul2100")
+    hmel = load_gff3("data/Hmel202001o.test.gff3", species="Hmel")
+    eisa = load_gff3("data/Eisa2100.test.gff3", species="Eisa")
+    diul = load_gff3("data/Diul2100.test.gff3", species="Diul")
 
-    hmel_loci = build_species_loci(list(hmel.values()), species="Hmel202001o")
-    eisa_loci = build_species_loci(list(eisa.values()), species="Eisa2100")
-    diul_loci = build_species_loci(list(diul.values()), species="Diul2100")
+    hmel_loci = build_species_loci(list(hmel.values()), species="Hmel")
+    eisa_loci = build_species_loci(list(eisa.values()), species="Eisa")
+    diul_loci = build_species_loci(list(diul.values()), species="Diul")
 
     species_loci = {
-        "Hmel202001o": hmel_loci,
-        "Eisa2100": eisa_loci,
-        "Diul2100": diul_loci,
+        "Hmel": hmel_loci,
+        "Eisa": eisa_loci,
+        "Diul": diul_loci,
     }
 
     seed = hmel["transcript:Hmel202001oG1.1"]
 
-    hal = HALAdapter("/mnt/data/3SpChr21.hal")
+    hal = HALAdapter("data/3SpChr21.hal")
 
-    for target in ["Eisa2100", "Diul2100"]:
+    for target in ["Eisa", "Diul"]:
         clocus = infer_comparative_locus(
             seed_transcript=seed,
             target_species=target,
