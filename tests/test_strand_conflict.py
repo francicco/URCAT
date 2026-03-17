3c812887f588250e08a2a0c7b3a524eee1a11291
Francesco Cicconardi <francicco@gmail.com>
2026-03-17 11:30:06 +0000

    Update test_strand_conflict.py

--- tests/test_strand_conflict.py
+++ tests/test_strand_conflict.py
@@ -1,9 +1,23 @@
+from comparative_annotator.models.transcript import CandidateTranscript
 from comparative_annotator.models.projected_transcript import ProjectedTranscript
 from comparative_annotator.models.locus import SpeciesLocus
 from comparative_annotator.loci.comparative_builder import build_comparative_locus_from_projection
 
 
 def test_strand_conflict_candidate():
+    source = CandidateTranscript(
+        transcript_id="tx1",
+        species="Hmel",
+        seqid="chr1",
+        start=100,
+        end=200,
+        strand="+",
+        source="test",
+        exons=[(100, 150)],
+        cds=[],
+    )
+    source.finalize()
+
     projected = ProjectedTranscript(
         species="Diul",
         seqid="Diul2100",
@@ -12,6 +26,7 @@ def test_strand_conflict_candidate():
         source_transcript="tx1",
         exons=[(1000, 1100)],
         coverage=1.0,
+        source_exon_indices=[0],
     )
 
     locus = SpeciesLocus(
@@ -29,6 +44,8 @@ def test_strand_conflict_candidate():
         seed_transcript="tx1",
         projected_transcripts=[projected],
         species_loci={"Diul": [locus]},
+        source_transcript=source,
+        transcripts_by_species={},
     )
 
     assert "Diul" in clocus.strand_conflicts
