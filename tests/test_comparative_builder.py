79273b7203558794312d80194c8ceabe863deaee
Francesco Cicconardi <francicco@gmail.com>
2026-03-17 11:29:49 +0000

    Update test_comparative_builder.py

--- tests/test_comparative_builder.py
+++ tests/test_comparative_builder.py
@@ -1,9 +1,22 @@
+from comparative_annotator.models.transcript import CandidateTranscript
 from comparative_annotator.models.projected_transcript import ProjectedTranscript
 from comparative_annotator.models.locus import SpeciesLocus
 from comparative_annotator.loci.comparative_builder import build_comparative_locus_from_projection
 
 
 def test_build_comparative_locus():
+    source = CandidateTranscript(
+        transcript_id="tx1",
+        species="Heliconius",
+        seqid="chr1",
+        start=100,
+        end=300,
+        strand="+",
+        source="test",
+        exons=[(100, 150), (200, 250)],
+        cds=[],
+    )
+    source.finalize()
 
     projected = ProjectedTranscript(
         species="Dryas",
@@ -11,7 +24,9 @@ def test_build_comparative_locus():
         strand="+",
         source_species="Heliconius",
         source_transcript="tx1",
-        exons=[(1000,1050),(1100,1150)]
+        exons=[(1000, 1050), (1100, 1150)],
+        coverage=1.0,
+        source_exon_indices=[0, 1],
     )
 
     locus = SpeciesLocus(
@@ -24,13 +39,15 @@ def test_build_comparative_locus():
         transcripts=["txA"]
     )
 
-    species_loci = {"Dryas":[locus]}
+    species_loci = {"Dryas": [locus]}
 
     clocus = build_comparative_locus_from_projection(
         "Heliconius",
         "tx1",
         [projected],
-        species_loci
+        species_loci,
+        source_transcript=source,
+        transcripts_by_species={},
     )
 
     assert "Dryas" in clocus.primary
