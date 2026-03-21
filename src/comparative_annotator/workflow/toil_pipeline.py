169870a5232e535fabe6081e68f545183f56eae1
Francesco Cicconardi <francicco@gmail.com>
2026-03-21 17:39:34 +0000

    Update toil_pipeline.py

--- src/comparative_annotator/workflow/toil_pipeline.py
+++ src/comparative_annotator/workflow/toil_pipeline.py
@@ -38,6 +38,7 @@ from comparative_annotator.workflow.analysis_tables import (
 )
 
 from comparative_annotator.workflow.final_gff3 import write_all_final_species_gff3
+from comparative_annotator.workflow.final_gff3 import write_final_species_gff3s
 
 from comparative_annotator.workflow.new_loci_gff3 import (
     write_round_new_loci_gff3,
